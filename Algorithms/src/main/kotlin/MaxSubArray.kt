7179b8d650e323624358f405b8f4ea42af82af85
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-10-26 01:50:37 +0300

    Implement MaxSubArray.kt (accepted but needs refactor)

--- Algorithms/src/main/kotlin/MaxSubArray.kt
+++ Algorithms/src/main/kotlin/MaxSubArray.kt
@@ -1,12 +1,51 @@
 class MaxSubArray {
     fun maxSubArray(nums: IntArray): Int {
-        var maxSum = nums.first()
-        val l = nums.size
-        for (offset in 0 until l) {
-            for (sL in offset until l) {
-                maxSum = maxOf(nums.slice(offset..sL).sum(), maxSum)
+        val n = nums.size
+        if (n == 1) return nums[0]
+
+        var currentMax = nums.first()
+        val possibleMax = mutableListOf<Int>()
+
+        for (i in 1 until n) {
+            val num = nums[i]
+            if (num > 0) {
+                // positive number
+                if (currentMax > 0) {
+                    // positive sum => increment with positive number
+                    currentMax += num
+                } else {
+                    // negative sum => replace with the new positive number
+                    currentMax = num
+                }
+            } else {
+                // negative number
+                if (currentMax > 0) {
+                    // positive sum
+                    possibleMax.add(currentMax) // remember local max
+                    currentMax += num
+                } else {
+                    // negative sum
+                    if (num > currentMax) {
+                        // replace with the number
+                        currentMax = num
+                    } else {
+                        possibleMax.add(currentMax)
+                        currentMax += num
+                    }
+                }
             }
         }
-        return maxSum
+
+        return maxOf(possibleMax.toIntArray() + currentMax)
+    }
+
+    private fun maxOf(arr: IntArray): Int {
+        val n = arr.size
+        if (n == 1) return arr.first()
+        var max = arr.first()
+        for (i in 1 until n) {
+            max = maxOf(arr[i], max)
+        }
+        return max
     }
 }
--- Algorithms/src/test/kotlin/MaxSubArrayTest.kt
+++ Algorithms/src/test/kotlin/MaxSubArrayTest.kt
@@ -9,10 +9,13 @@ class MaxSubArrayTest : FreeSpec({
             nameFn = { (input, sum) ->
                 "${input.toList()} should be $sum"
             },
+            row(intArrayOf(2, 1, -5, 4), 4),
             row(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4), 6),
+            row(intArrayOf(1,2,3,4), 10),
             row(intArrayOf(1), 1),
             row(intArrayOf(5, 4, -1, 7, 8), 23),
             row(intArrayOf(-2,-1), -1),
+            row(intArrayOf(1,2,-1,-2,2,1,-2,1,4,-5,4), 6)
         ) { (input, output) ->
             val sut = MaxSubArray()
 
