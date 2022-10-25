c47ded04140fe171dbfe3ccba6ae9a11e0500faf
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-10-26 02:07:56 +0300

    Refactor and optimize MaxSubArray.kt

--- Algorithms/src/main/kotlin/MaxSubArray.kt
+++ Algorithms/src/main/kotlin/MaxSubArray.kt
@@ -1,49 +1,60 @@
+/**
+ * Problem:
+ * https://leetcode.com/problems/maximum-subarray/
+ */
 class MaxSubArray {
     fun maxSubArray(nums: IntArray): Int {
-        val n = nums.size
-        if (n == 1) return nums[0]
+        val size = nums.size
+        if (size == 1) return nums[0]
 
-        var currentMax = nums.first()
-        val possibleMax = mutableListOf<Int>()
+        var sum = nums.first()
+        val possibleMaxes = ArrayList<Int>(size)
 
-        for (i in 1 until n) {
-            val num = nums[i]
-            if (num > 0) {
-                // positive number
-                if (currentMax > 0) {
-                    // positive sum => increment with positive number
-                    currentMax += num
+        for (i in 1 until size) {
+            val n = nums[i]
+            when {
+                n > 0 -> if (sum > 0) {
+                    // n(+) + sum(+) => continue the sum: n + sum
+                    sum += n
                 } else {
-                    // negative sum => replace with the new positive number
-                    currentMax = num
+                    // n(+) + sum(-) => reset to n(+)
+                    sum = n
                 }
-            } else {
-                // negative number
-                if (currentMax > 0) {
-                    // positive sum
-                    possibleMax.add(currentMax) // remember local max
-                    currentMax += num
-                } else {
-                    // negative sum
-                    if (num > currentMax) {
-                        // replace with the number
-                        currentMax = num
+                else -> {
+                    if (sum > 0) {
+                        // n(-) + sum(+) => we can't know if we should reset the sum,
+                        // because we can't predict the next numbers coming
+                        // => 1) remember local max; 2) continue the sum;
+                        possibleMaxes.add(sum)
+                        sum += n
                     } else {
-                        possibleMax.add(currentMax)
-                        currentMax += num
+                        // sum(-)
+                        if (n > sum) {
+                            // n(-) is better than sum(-) => reset the sum to n(-)
+                            sum = n
+                        } else {
+                            // n(-) is worse than sum(-) but we can't predict the future
+                            // => 1) remember local max; 2) continue the sum;
+                            possibleMaxes.add(sum)
+                            sum += n
+                        }
                     }
                 }
             }
         }
 
-        return maxOf(possibleMax.toIntArray() + currentMax)
+        return if (possibleMaxes.isEmpty())
+            sum else maxOf(max(possibleMaxes), sum)
     }
 
-    private fun maxOf(arr: IntArray): Int {
-        val n = arr.size
-        if (n == 1) return arr.first()
+    /**
+     * LeetCode doesn't know the "List.maxOf{}" built-in function
+     * so we have to create our own one.
+     */
+    private fun max(arr: ArrayList<Int>): Int {
         var max = arr.first()
-        for (i in 1 until n) {
+        val size = arr.size
+        for (i in 1 until size) {
             max = maxOf(arr[i], max)
         }
         return max
