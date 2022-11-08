914c8230d0170e56379c61273b5d78ff1e09b0fd
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-11-08 16:01:13 +0200

    Test

---
+++ Algorithms/src/main/kotlin/AlgoExpert.kt
@@ -0,0 +1,54 @@
+fun counts(teamA: Array<Int>, teamB: Array<Int>): Array<Int> {
+    teamA.sort()
+    val aSize = teamA.size
+    val bSize = teamB.size
+
+    for (i in 0 until bSize) {
+        val goalsB = teamB[i]
+        teamB[i] = search(
+            teamA = teamA,
+            teamBGoals = goalsB,
+            latestIndex = null,
+            left = 0,
+            right = aSize - 1
+        )?.plus(1) ?: 0
+    }
+
+    return teamB
+}
+
+// teamA = [1, 2, 3, 4, 5]
+// teamB = [1, 6]
+
+private fun search(
+    teamA: Array<Int>,
+    teamBGoals: Int,
+    latestIndex: Int?,
+    left: Int,
+    right: Int
+): Int? {
+    if (left > right) return latestIndex
+
+    val mid = (left + right) / 2
+    val aGoals = teamA[mid]
+
+    return if (teamBGoals >= aGoals) {
+        // search right
+        search(
+            teamA = teamA,
+            teamBGoals = teamBGoals,
+            latestIndex = mid,
+            left = mid + 1,
+            right = right
+        )
+    } else {
+        // team B scored less
+        search(
+            teamA = teamA,
+            teamBGoals = teamBGoals,
+            latestIndex = mid.takeIf { latestIndex != null },
+            left = 0,
+            right = mid - 1
+        )
+    }
+}
